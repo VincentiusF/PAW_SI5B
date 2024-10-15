5bbdee4cf14da392b2198538aca4fbbdbb59a4c3
Vincentius Fernando <vincentiusfernando_2226240032@mhs.mdp.ac.id>
2024-10-15 15:17:14 +0700

    API 1

--- API/routes/bukus.js
+++ API/routes/bukus.js
@@ -27,5 +27,41 @@ router.post('/', (req, res) => {
     });   
 });
 
+router.get("/",(req, res) => {
+    Buku.find()
+    .then((documents)=>{
+        res.status(201).json({
+            message : "Get data buku",
+            bukus : documents
+        });
+    });
+})
+
+router.delete('/:id', (req, res) => {
+    Buku.deleteOne({_id : req.params.id})
+    .then((result)=>{
+        res.status(200).json({
+            message : "Buku berhasil dihapus",
+            result : result
+        });
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const buku = new Buku({
+        _id : req.params.id,
+        judul : req.body.judul,
+        penulis : req.body.penulis,
+        genre : req.body.genre
+    });
+
+    Buku.updateOne({_id : req.params.id}, buku)
+    .then((hasil)=>{
+        res.status(200).json({
+            message : "Update berhasil",
+            result : hasil
+        });
+    });
+});
 
 module.exports = router;
