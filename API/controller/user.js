5f0348f24235ed295632d801d4be6ea7e89911e6
Vincentius Fernando <vincentiusfernando_2226240032@mhs.mdp.ac.id>
2024-10-24 13:05:39 +0700

    pert 14

--- API/controller/user.js
+++ API/controller/user.js
@@ -1,5 +1,63 @@
-const user = require("../model/user");
+const User = require("../model/user");
+const bcrypt = require('bcrypt');
 
-const signUp = (req, res)=>{
-    
+const signUp = (req, res) => {
+    bcrypt.hash(req.body.password, 10)
+        .then((hash) => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+
+            return user.save();
+        })
+        .then((result) => {
+            res.status(201).json({
+                message: "User Created"
+                // result: result
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: "Internal server error",
+                error: err
+            });
+        });
 };
+
+const login = (req, res) => {
+    let fetchedUser;
+
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (!user) {
+                return res.status(401).json({
+                    message: "Auth failed, email does not exist"
+                });
+            }
+
+            fetchedUser = user;
+            return bcrypt.compare(req.body.password, user.password);
+        })
+        .then((result) => {
+            if (!result) {
+                return res.status(401).json({
+                    message: "Auth failed, password incorrect"
+                });
+            }
+
+            // Here, you can generate a token or respond with success
+            res.status(200).json({
+                message: "Auth successful",
+                user: fetchedUser
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: "Internal server error",
+                error: err
+            });
+        });
+};
+
+module.exports = { signUp, login };
--- API/routes/users.js
+++ API/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const userController = require('../controller/user');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.post('/', userController.signUp);
+
+router.post('/login', userController.login);
 
 module.exports = router;
