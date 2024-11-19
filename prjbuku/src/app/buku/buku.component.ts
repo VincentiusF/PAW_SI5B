59668b0d518760074ceccb1d9196da0209f59f97
VincentiusF <vincentiusfernando_2226240032@mhs.mdp.ac.id>
2024-11-19 14:55:06 +0700

    pert 12

--- prjbuku/src/app/buku/buku.component.ts
+++ prjbuku/src/app/buku/buku.component.ts
@@ -1,15 +1,44 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { BukuService } from '../services/buku.service';
 @Component({
   selector: 'app-buku',
   templateUrl: './buku.component.html',
   styleUrl: './buku.component.css'
 })
 export class BukuComponent {
+  constructor(public bukuService : BukuService) {
+
+  }
+
+
+
   simpanBuku(form : NgForm){
+
+    if(form.invalid) {
+      console.log("Tidak Valid")
+      alert("Data tidak valid")
+      return;
+    }
+
+    let genres : string[] = [];
+
+    if (form.value.genre1 == true){
+      genres.push("Biografi")
+    }
+
+    if(form.value.genre2 == true){
+      genres.push("Pendidikan")
+    }
+
+    if(form.value.genre2 == true){
+      genres.push("Romance")
+    }
     console.log("Pengujian Klik")
     console.log(form.value.judul);
     console.log(form.value.penulis);
-    // console.log(form.value.genre1);
+    console.log(genres);
+
+    this.bukuService.addBuku(form.value.judul)
   }
 }
