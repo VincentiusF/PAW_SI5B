be28ea107a5e5a94808a6098272a39e86f212f6c
VincentiusF <vincentiusfernando_2226240032@mhs.mdp.ac.id>
2024-11-21 18:50:49 +0700

    update db.js

--- API/app_server/model/db.js
+++ API/app_server/model/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    'mongodb://localhost:27017/dbbuku'
+  'mongodb://127.0.0.1:27017/dbbuku'
+    // 'mongodb://localhost:27017/dbbuku'
     // 'mongodb+srv://mdp:Vf29122003@cluster0.eyoha.mongodb.net/dbbuku?retryWrites=true&w=majority&appName=Cluster0'
   ).then(() => {
     console.log('Connected to database');
